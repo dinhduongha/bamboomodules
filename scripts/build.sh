3a03f1577071a47c24ca6126858bd2b015c27693
PiConnect Dev <piconnectdev@gmail.com>
2024-06-14 17:45:51 +0700

    update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,19 @@ name="Bamboo"
 #rm Output/$name.AuthServer/appsettings.secret*.*
 #dotnet publish --sc --os linux -o Output/$name.Admin services/admin/$name.Admin/src/$name.Admin.HttpApi.Host/$name.Admin.HttpApi.Host.csproj
 #rm Output/$name.Admin/appsettings.secret*.*
+
 dotnet publish --sc --os linux -o Output/$name.Admin services/admin/$name.Admin/$name.Admin.sln
 rm Output/$name.Admin/appsettings.secret*.*
 
-dotnet publish --sc --os linux -o Output/Bamboo.MVC apps/Bamboo.Web.MVC.sln
-rm Output/Bamboo.MVC/appsettings.secret*.*
+# dotnet publish --sc --os linux -o Output/Bamboo.Admin services/admin/Bamboo.Admin/Bamboo.Admin.sln
+# rm Output/Bamboo.Admin/appsettings.secret*.*
+# dotnet publish --sc --os linux -o Output/Bamboo.MVC apps/Bamboo.Web.MVC.sln
+# rm Output/Bamboo.MVC/appsettings.secret*.*
+
+# dotnet publish --sc --os linux -o Output/Bamboo.Core services/core/Bamboo.Core.sln
+# rm Output/Bamboo.Core/appsettings.secret*.*
+
+# dotnet publish --sc --os linux -o Output/Bamboo.Blazor apps/Bamboo.Web.Blazor.sln
 
 # scp -r Output/$name.AuthServer/$name.* bamboo@127.0.0.1:/opt/bamboo/admin/dotnet/
 # scp -r Output/$name.Admin/$name.* bamboo@127.0.0.1:/opt/bamboo/admin/dotnet/
--- scripts/db-migrate-core.sh
+++ scripts/db-migrate-core.sh
@@ -2,6 +2,20 @@
 name="Bamboo"
 module="Core"
 path="core"
-dotnet ef migrations add Upgrade_ABP_8 --startup-project services/$path/host/$name.$module.HttpApi.Host/$name.$module.HttpApi.Host.csproj --project services/core/src/$name.$module.EntityFrameworkCore/$name.$module.EntityFrameworkCore.csproj --context $module"DbContext"
 
+# Migration Entities
+# dotnet ef migrations add Initial --startup-project services/$path/host/$name.$module.HttpApi.Host/$name.$module.HttpApi.Host.csproj --project services/$path/src/$name.$module.EntityFrameworkCore/$name.$module.EntityFrameworkCore.csproj --context $module"DbContext"
+
+#dotnet ef migrations add Upgrade_ABP_8 --startup-project services/$path/host/$name.$module.HttpApi.Host/$name.$module.HttpApi.Host.csproj --project services/$path/src/$name.$module.EntityFrameworkCore/$name.$module.EntityFrameworkCore.csproj --context $module"DbContext"
+
+# Update database
+dotnet ef database update --startup-project services/$path/host/$name.$module.HttpApi.Host/$name.$module.HttpApi.Host.csproj --project services/$path/src/$name.$module.EntityFrameworkCore/$name.$module.EntityFrameworkCore.csproj --context $module"DbContext"
+
+
+# SCAFFOLD DATA
+# MY SQL
+#dotnet ef dbcontext scaffold 'Server=host; port=port;Database=db;User=user;Password=password;' "Pomelo.EntityFrameworkCore.MySql" --namespace "$name"."$module".Entities -o ../"$name"."$module".Domain/Entities -c "$module"DbContext --startup-project host/"$name"."$module".HttpApi.Host/"$name"."$module".HttpApi.Host.csproj --project src/"$name"."$module".EntityFrameworkCore/"$name"."$module".EntityFrameworkCore.csproj  --context-dir "$module"DbContext --force #--data-annotations
+
+# PostgreSQL
+#dotnet ef dbcontext scaffold 'Host=host; Port=port;Database=db;User ID=user;Password=password;' "Npgsql.EntityFrameworkCore.PostgreSQL" -o Entities -c "$module"DbContext --startup-project host/"$name"."$module".HttpApi.Host/"$name"."$module".HttpApi.Host.csproj --project src/"$name"."$module".EntityFrameworkCore/"$name"."$module".EntityFrameworkCore.csproj --context-dir "$module"DbContext --force --data-annotations
 
