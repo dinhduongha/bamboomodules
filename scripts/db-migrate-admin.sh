8ec87768687e52c14332132445926814c68b640d
PiConnect Dev <piconnectdev@gmail.com>
2024-07-13 10:27:43 +0700

    update script

--- scripts/db-migrate-admin.sh
+++ scripts/db-migrate-admin.sh
@@ -4,4 +4,4 @@ module="Admin"
 path="admin"
 dotnet ef migrations add Upgrade_ABP_8 --startup-project services/$path/$name.$module/src/$name.$module.HttpApi.Host/$name.$module.HttpApi.Host.csproj --project services/$path/$name.$module/src/$name.$module.EntityFrameworkCore/$name.$module.EntityFrameworkCore.csproj --context $module"DbContext"
 
-
+#abp create-migration-and-run-migrator Bamboo.Admin.EntityFrameworkCore
