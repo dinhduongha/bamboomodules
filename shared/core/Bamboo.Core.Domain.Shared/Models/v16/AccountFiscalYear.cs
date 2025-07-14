c0dc9664ae2dffd7973e8764d401b162ed9abc2d
Dinh Duong Ha <dinhduongha@gmail.com>
2025-07-14 11:42:37 +0700

    Move to v16 folder

---
+++ shared/core/Bamboo.Core.Domain.Shared/Models/v16/AccountFiscalYear.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Auditing;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace Bamboo.Core.Models;
+
+[Table("account_fiscal_year")]
+public partial class AccountFiscalYear: FullAuditedEntity<Guid>, IEntityDto<Guid>, IMultiTenant, IAuditedObject
+{
+    [Key]
+    [Column("id")]
+    public Guid Id { get => base.Id; set => base.Id = value; }
+
+    [Column("company_id")]
+    public Guid? TenantId { get; set; }
+
+    [Column("create_uid")]
+    public Guid? CreatorId { get; set; }
+
+    [Column("write_uid")]
+    public Guid? LastModifierId { get; set; }
+
+    [Column("name")]
+    public string? Name { get; set; }
+
+    [Column("date_from")]
+    public DateTime? DateFrom { get; set; }
+
+    [Column("date_to")]
+    public DateTime? DateTo { get; set; }
+
+    [Column("create_date", TypeName = "timestamp without time zone")]
+    public DateTime CreationTime { get; set; }
+
+    [Column("write_date", TypeName = "timestamp without time zone")]
+    public DateTime? LastModificationTime { get; set; }
+
+    [ForeignKey("TenantId")]
+    //[InverseProperty("AccountFiscalYears")]
+    [NotMapped]
+    public virtual ResCompany? Company { get; set; }
+
+    [ForeignKey("CreatorId")]
+    //[InverseProperty("AccountFiscalYearCreateUs")]
+    [NotMapped]
+    public virtual ResUser? CreateU { get; set; }
+
+    [ForeignKey("LastModifierId")]
+    //[InverseProperty("AccountFiscalYearWriteUs")]
+    [NotMapped]
+    public virtual ResUser? WriteU { get; set; }
+}
